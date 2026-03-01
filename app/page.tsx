378fb4408d56a44f6fc936e7dcc6ce112e14e0c8
w4click <suportechnet2021@gmail.com>
2026-03-01 16:55:15 -0300

    add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Login } from '@/components/Login';
+import { Dashboard } from '@/components/Dashboard';
+import { NotificationProvider } from '@/components/NotificationContext';
+*/
